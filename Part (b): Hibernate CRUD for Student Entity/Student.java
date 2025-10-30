077ee0d37a95f3ae4207b6fd37879480b4ad5d54
harshwardhan023 <harshwardhankumar23@gmail.com>
2025-10-30 12:09:12 +0530

    Create Student.java

---
+++ Part (b): Hibernate CRUD for Student Entity/Student.java
@@ -0,0 +1,19 @@
+package com.example.hibernate;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    // getters, setters, toString()
+}
